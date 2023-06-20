7f2e0ae5fb24f8c62c6c5faa920b52919fb1d680
stc-c22001 <c22001@suntech.jp>
2023-06-20 10:07:55 +0900

    Initial commit

---
+++ app/src/main/java/jp/suntech/c22001/myhello/MainActivity.java
@@ -0,0 +1,39 @@
+package jp.suntech.c22001.myhello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btClick=findViewById(R.id.btClick);
+        HelloListener listener=new HelloListener();
+        btClick.setOnClickListener(listener);
+
+    }
+    private class HelloListener implements View.OnClickListener {
+
+
+        @Override
+        public void onClick(View v) {
+            EditText input = findViewById(R.id.etName);
+            TextView output = findViewById(R.id.tvOutput);
+
+
+            String inputStr = input.getText().toString();
+            output.setText(inputStr + "さん、こんにちは");
+
+        }
+
+    }
+
+}
